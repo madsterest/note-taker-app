470361225e3bbd3b4e8450408c0a22b9c62818ad
madsterest <odea.madeleine@gmail.com>
2021-05-17 14:54:12 +1000

    Fixed the bug

--- server.js
+++ server.js
@@ -1,45 +1,48 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
+const uniqid = require("uniqid");
 
 app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, "public")));
-
-fs.readFile("./db/db.json", "utf8", (err, data) => {
-  let noteData = JSON.parse(data);
-  console.log(typeof noteData, noteData);
-
-  app.get("/api/notes", (req, res) => {
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    let noteData = JSON.parse(data);
     res.json(noteData);
-  });
-
-  app.post("/api/notes", (req, res) => {
-    const note = {
-      id: data.length + 1,
-      title: req.body.title.trim(),
-      text: req.body.text.trim(),
-    };
-
-    let newNoteData = noteData.concat(note);
-
-    fs.writeFile("./db/db.json", JSON.stringify(newNoteData), (err, data) => {
-      if (err) throw err;
-      res.json(data);
-    });
   });
 });
 
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    let noteData = JSON.parse(data);
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uniqid(),
+    };
+
+    const newNoteData = noteData.concat(newNote);
+    modifyDB(newNoteData);
+    res.json(newNoteData);
+  });
+});
+
+function modifyDB(modify) {
+  fs.writeFile("./db/db.json", JSON.stringify(modify), (err) => {
+    if (err) throw err;
+  });
+}
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.listen(PORT, () => {
