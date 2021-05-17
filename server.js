3b8649a4dd219f3def64bf2899e355ab6a92f6ea
madsterest <odea.madeleine@gmail.com>
2021-05-17 11:55:08 +1000

    Recopied the original files. Added express.static. Solved json parsing issue

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const express = require("express");
 const path = require("path");
 
 app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 fs.readFile("./db/db.json", "utf8", (err, data) => {
   let noteData = JSON.parse(data);
+  console.log(typeof noteData, noteData);
 
   app.get("/api/notes", (req, res) => {
     res.json(noteData);
@@ -16,20 +21,25 @@ fs.readFile("./db/db.json", "utf8", (err, data) => {
   app.post("/api/notes", (req, res) => {
     const note = {
       id: data.length + 1,
-      title: req.body.title,
-      text: req.body.text,
+      title: req.body.title.trim(),
+      text: req.body.text.trim(),
     };
-    noteData.push(note);
-    res.json(noteData);
+
+    let newNoteData = noteData.concat(note);
+
+    fs.writeFile("./db/db.json", JSON.stringify(newNoteData), (err, data) => {
+      if (err) throw err;
+      res.json(data);
+    });
   });
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.listen(PORT, () => {
